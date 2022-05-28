d83130515a95ef676832aa36e32dab622004a3ce
PedroIeremis <pedroieremis@gmail.com>
2022-05-28 18:46:09 -0300

    Atualização

--- Verficador_IP_Extr.py
+++ Verficador_IP_Extr.py
@@ -8,7 +8,7 @@ resposta = urlopen(url)
 dados = json.load(resposta)
 
 
-dados_filtados.append('INFORMAÇÕES SUAS:')
+dados_filtados.append('SUAS INFORMAÇÕES:')
 dados_filtados.append('IP EXTERNO: '+dados['ip'])
 dados_filtados.append('HOST: '+dados['hostname'])
 dados_filtados.append('PAÍS: '+dados['country'])
@@ -17,7 +17,8 @@ dados_filtados.append('CIDADE: '+dados['city'])
 
 dados_filtados.append('\nINFORMAÇÕES DO SEU FORNECEDOR:')
 dados_filtados.append('PROVEDOR: '+dados['org'])
-dados_filtados.append('LOCALIZAÇÃO PROVEDOR: '+dados['timezone'])
+dados_filtados.append('LOCALIZAÇÃO: '+dados['postal'])
+dados_filtados.append('FUSO-HORÁRIO: '+dados['timezone'])
 
 print()
 for i in dados_filtados:
