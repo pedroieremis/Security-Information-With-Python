f6d421949460179ffc2fd54e0e4c7521b2a7128b
PedroIeremis <pedroieremis@gmail.com>
2022-07-08 12:38:17 -0300

    Update

--- For Networks/Search_IP_Public.sh
+++ For Networks/Search_IP_Public.sh
@@ -3,7 +3,4 @@
 
 res=$(curl -s "https://ipinfo.io/json" | tr -d "{" | cut -d ":" -f 2 | head -n 2 | tr -d '"' | tr -d ' ' | tr -d ',' | tr -d '\n')
 
-echo $res
-rm arq.sip
-cp arq.sip.resv arq.sip
-sed -i "s/ippublico/$res/" arq.sip
+echo "Your Public IP is > $res"
