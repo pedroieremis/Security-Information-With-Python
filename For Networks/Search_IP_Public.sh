ebeec2d3b4cbc6ea195b75e9c4178dc012f8ccd6
Pedro Iéremis <104873962+pedroieremis@users.noreply.github.com>
2022-11-30 10:50:47 -0300

    Delete Search_IP_Public.sh

--- For Networks/Search_IP_Public.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-res=$(curl -s "https://ipinfo.io/json" | tr -d "{" | cut -d ":" -f 2 | head -n 2 | tr -d '"' | tr -d ' ' | tr -d ',' | tr -d '\n')
-
-echo "Your Public IP is > $res"
